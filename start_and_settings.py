5c8c8db0b27ba113b796971eae54f24af93453d1
Igor Petrov <ds9657@yandex.ru>
2023-02-21 14:15:27 +0600

    Удалён ненужный код, изменена логика
    Удалён устаревший код.
    Логика работы программы изменена: теперь передаются целевые элементы страницы для нажатия в цикл обработки.

--- start_and_settings.py
+++ start_and_settings.py
@@ -8,35 +8,16 @@ def main():
     print('Читаем настройки')
     with open('settings.txt', 'r', encoding='utf-8') as settings:
         sets = settings.read().split('\n')
-        link, login, password, mode = sets[0], sets[1], sets[2], sets[3]
+        link, login, password = sets[0], sets[1], sets[2]
         print(f'Настройки прочитаны:\n',
               f'link: {link}\n',
               f'login: {login}\n',
-              f'password: {password}\n',
-              f'work_mode: {mode}')  # режим запуска скрипта: 1 - одна кнопка, 2 - много кнопок
+              f'password: {password}\n')
 
     # Создаём экземпляр класса и запускаем скрипт движка работы с сайтом Head Hunter
-    # Возвращаем ЭК для дальнейшей работы с ним
-    run_instance = autorefresher.Engine(link, login, password, mode)
-    return run_instance
+    run_instance = autorefresher.Engine(link, login, password)
+
 
 
 if __name__ == "__main__":
     main()
-    print(type(main()))
-
-
-
-
-
-
-    # logger.add("debug.json", format="{time} {level} {message}",
-    #            level='DEBUG', rotation="10 KB", serialize=True)
-    # также параметр rotation поддерживает: вес файла - "10 МВ",
-    # период обновления - "1 week", время обновления = "10:00"
-    # компрессию zip: compression='zip'
-    # serialize=True - поддержка json в дампе, debug.log лучше
-    # переименовать в debug.json
-    # декоратор @logger.catch поддерживает traceback исключений в файл дампа
-
-
